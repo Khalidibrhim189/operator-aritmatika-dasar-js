118f5c18fde24848d84a82210f0a19f53ca33921
Khalid Ibrahim <khalid.jambi18@gmail.com>
2021-03-22 07:45:11 +0700

    operator matematika javascript

---
+++ data.js
@@ -0,0 +1,51 @@
+//Mengenal operator matematika pada javascript
+// penjumlahan (+)
+//pengurangan (-)
+//perkalian (*)
+//pemangkatan( ** )
+//pembagian(/)
+//sisa hasil/modul ( % )
+
+// contoh operator aritmatika dengan penjumlahan
+var angka1 = 10;
+var angka2 = 30;
+var hasil  =  0;
+
+//penjumlahan 
+hasil = angka1 + angka2;
+document.write(` ${angka1} + ${angka2} = ${hasil} <br>`);
+
+//pengurangan
+var a       = 10;
+var b       =  5;
+var hasil1  =  0;
+
+hasil1      = a - b;
+document.write(` ${a} - ${b} = ${hasil1} <br>`);
+
+//perkalian
+var c = 10;
+var d = 20;
+var hasiljumlah = 0;
+
+
+hasiljumlah = c * d;
+document.write(`  ${c} * ${d} = ${hasiljumlah} <br>`);
+
+//pembagian
+var e = 100;
+var f =  50;
+
+var hasilbagi = 0;
+
+hasilbagi = e / f;
+document.write(` ${e} / ${f} = ${hasilbagi} <br>`);
+
+//moduls
+var has1 = 100;
+var has2 = 50;
+var hasilGabung = 0;
+
+
+hasilGabung = has1 % has2;
+document.write(` ${has1} % ${has2} = ${hasilGabung} `);
